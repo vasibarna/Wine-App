4a054be02f4a27ec17a47d7f1ef351dc7483ecfe
vasile <vasi_barna@yahoo.com>
2020-07-23 21:15:43 +0300

    update

--- wine_app/models.py
+++ wine_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 class Categorie(models.Model):
     nume = models.CharField(max_length = 30)
@@ -13,8 +16,20 @@ class Vin(models.Model):
     pret = models.FloatField()
     nota = models.FloatField()
     def __str__(self):
-        return f'{self.nume} at {self.pret} lei with image {self.imagine} and category {self.categorie} has a rating of {self.nota}.'
+        return f'{self.nume} at {self.pret} lei with category {self.categorie} has a rating of {self.nota}.'
 
 class Picture(models.Model):
     vin = models.ForeignKey('wine_app.Vin',on_delete=models.DO_NOTHING)
     picture = models.ImageField(upload_to='images', default="null")
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.picture.path)
+        width, height = img.size
+        new_height = 900
+        new_width  = new_height * width / height
+        output_size = (new_width, new_height)
+        img.thumbnail(output_size)
+        img.save(self.picture.path)
+
+
